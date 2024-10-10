6a548bb36177e50a4acd3e27efbe12f2eda44285
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-10-11 00:39:01 +0300

    header ve ilk 2 section alanları eklendi.

---
+++ assets/scripts/app.js
@@ -0,0 +1,232 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const siteViewport = document.querySelector(".page-wrapper");
+  const navigationMenu = document.querySelector(".header .menu-nav");
+  const mobileNavigation = createMobileNavigation();
+  const touchLinks = getTouchLinks();
+
+  touchLinks.forEach((link) => link.addEventListener("click", preventDefault));
+
+  document.body.appendChild(mobileNavigation);
+
+  const menuWrapper = createMenuWrapper();
+  mobileNavigation.querySelector(".navigation-menu").appendChild(menuWrapper);
+
+  const dropdowns = document.querySelectorAll(".dropdown");
+  const mobileOverlay = createMobileOverlay();
+  const menuToggle = document.querySelectorAll(".btn-menu, .mobile-overlay");
+
+  mobileNavigation.appendChild(mobileOverlay);
+
+  dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener("click", () => toggleDropdown(dropdown));
+  });
+
+  if (navigationMenu) {
+    const clonedMenu = navigationMenu.cloneNode(true);
+    menuWrapper.appendChild(clonedMenu);
+  }
+
+  menuToggle.forEach((toggle) => {
+    toggle.addEventListener("click", () =>
+      toggleMobileNavigation(siteViewport)
+    );
+  });
+});
+
+function createMobileNavigation() {
+  const mobileNavigation = document.createElement("div");
+  mobileNavigation.classList.add("mobile-navigation");
+  mobileNavigation.innerHTML = '<div class="navigation-menu"></div>';
+  return mobileNavigation;
+}
+
+function getTouchLinks() {
+  return document.querySelectorAll(
+    ".header .navigation-menu .nav > ul > li.accordion > a, .header .navigation-menu .nav > ul > li.mobile-menu > a"
+  );
+}
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+function createMenuWrapper() {
+  const menuWrapper = document.createElement("div");
+  menuWrapper.classList.add("menu-wrapper");
+  menuWrapper.innerHTML = `
+      <button class="btn-menu" type="button">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <nav class="nav-links">
+        <ul>
+          ${getMenuItemsHTML()}
+        </ul>
+      </nav>
+    `;
+  return menuWrapper;
+}
+
+function getMenuItemsHTML() {
+  return `
+        <li class="dropdown">
+          <a href="#">Kurumsal</a>
+          <ul>${getCorporateMenuHTML()}</ul>
+        </li>
+        <li class="dropdown">
+          <a href="#">Ürünlerimiz</a>
+          <ul>${getProductsMenuHTML()}</ul>
+        </li>
+        <li class="dropdown">
+          <a href="#">Makarna & Sağlık</a>
+          <ul>${getHealthMenuHTML()}</ul>
+        </li>
+        <li class="dropdown">
+          <a href="#">Sosyal Medya</a>
+          <ul>${getSocialMediaMenuHTML()}</ul>
+        </li>
+        <li class="dropdown">
+          <a href="#">İhracat</a>
+          <ul>${getExportMenuHTML()}</ul>
+        </li>
+        <li class="dropdown">
+          <a href="#">İletişim</a>
+          <ul>${getContactMenuHTML()}</ul>
+        </li>
+      `;
+}
+
+function getCorporateMenuHTML() {
+  return `
+  <li><a href="#">Nuh Hakkında</a></li>
+  <li><a href="#">Markalar</a></li>
+  <li><a href="#">Nuh’tan Haberler</a></li>
+  <li><a href="#">Üretim Tesisi</a></li>
+  <li><a href="#">Kalite</a></li>
+  <li><a href="#">Sosyal Sorumluluk Projeleri</a></li>
+  <li><a href="#">İnsan Kaynakları</a></li>
+    `;
+}
+
+function getProductsMenuHTML() {
+  return `
+        <li><a href="#">Klasik</a></li>
+        <li><a href="#">Vitaminli</a></li>
+        <li><a href="#">Sebzeli</a></li>
+        <li><a href="#">Tam Buğday</a></li>
+        <li><a href="#">Bulgur</a></li>
+        <li><a href="#">Kepek</a></li>
+        <li><a href="#">Catering</a></li>
+        <li><a href="#">İrmik</a></li>
+        <li><a href="#">Glutensiz</a></li>
+      `;
+}
+
+function getHealthMenuHTML() {
+  return `
+        <li><a href="#">Makarna Şişmanlatmaz</a></li>
+        <li><a href="#">Makarnanın Öyküsü</a></li>
+        <li><a href="#">Buğdayın Öyküsü</a></li>
+        <li><a href="#">Besin Piramidi</a></li>
+        <li><a href="#">Makarnanın Püf Noktaları</a></li>
+        <li><a href="#">Sağlıklı Beslenme</a></li>
+        <li><a href="#">Faydalı Bilgiler</a></li>
+        <li><a href="#">Mevsiminde Yiyelim</a></li>
+      `;
+}
+
+function getSocialMediaMenuHTML() {
+  return `
+      <li><a href="#">Instagram</a></li>
+      <li><a href="#">Videolar</a></li>
+    `;
+}
+
+function getExportMenuHTML() {
+  return `
+        <li><a href="#">İhracat</a></li>
+        <li><a href="#">İhracat Formu</a></li>
+      `;
+}
+
+function getContactMenuHTML() {
+  return `
+        <li><a href="#">İletişim Bilgileri</a></li>
+        <li><a href="#">Bayi Başvuru Formu</a></li>
+      `;
+}
+
+function createMobileOverlay() {
+  const mobileOverlay = document.createElement("div");
+  mobileOverlay.classList.add("mobile-overlay");
+  return mobileOverlay;
+}
+
+function toggleDropdown(dropdown) {
+  const submenu = dropdown.querySelector("ul");
+  submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+}
+
+function toggleMobileNavigation(siteViewport) {
+  const duration = 400;
+  if (!document.documentElement.classList.contains("mobile-animating")) {
+    if (
+      document.documentElement.classList.contains("mobile-navigation-active")
+    ) {
+      document.documentElement.classList.add("mobile-animating");
+      document.documentElement.classList.remove("mobile-navigation-active");
+      setTimeout(() => {
+        document.documentElement.classList.remove(
+          "mobile-animation-ready",
+          "mobile-animating"
+        );
+        siteViewport.removeAttribute("style");
+      }, duration);
+    } else {
+      document.documentElement.classList.add(
+        "mobile-animation-ready",
+        "mobile-animating"
+      );
+      setTimeout(() => {
+        document.documentElement.classList.add("mobile-navigation-active");
+        setTimeout(() => {
+          document.documentElement.classList.remove("mobile-animating");
+        }, duration);
+      }, 30);
+    }
+  }
+}
+
+const swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + ' custom-bullet"></span>';
+    },
+  },
+});
+
+var swiperProduct = new Swiper(".mySwiperProduct", {
+  slidesPerView: "auto",
+  spaceBetween: 17,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+    1150: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
